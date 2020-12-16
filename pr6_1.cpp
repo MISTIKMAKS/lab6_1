c76d419c73ce679e12354e23e285412644af88c6
MISTIKMAKS <mistikmaks80885@gmail.com>
2020-12-16 20:25:59 +0200

    first master commit

---
+++ pr6_1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int A[25];
+	int P = 0;
+	for (int i = 0; i < 25; i++)
+	{
+		A[i] = rand() % 90 + 5;
+		if (i % 2 == 0)
+		{
+			if (i % 8 != 0)
+			{
+				P += A[i];
+			}
+		}
+	}
+	cout << "Array: ";
+	for (int i = 0; i < 25; i++)
+	{
+		cout << "[" << i << "] " << A[i] << "   ";
+	}
+	cout << "   Result = " << P;
+}
